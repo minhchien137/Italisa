925efdfbccdf09bf8a5b7d7f3db496127547b137
minhchien137 <bmc13072001@gmail.com>
2026-05-04 14:40:17 +0700

    Chỉnh lại usebase

--- obj/Release/net9.0/win-x64/ItalisaTools.AssemblyInfo.cs
+++ obj/Release/net9.0/win-x64/ItalisaTools.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ItalisaTools")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c160bea072ca6e0014b94506b264bfcb2e28b04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af284ae119800945a3f28dc75dfa37ea088903f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ItalisaTools")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ItalisaTools")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
