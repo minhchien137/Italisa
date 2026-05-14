5924ce70df8d5d0d82a738b82b44558dc4708b92
minhchien137 <bmc13072001@gmail.com>
2026-05-14 14:09:38 +0700

    Sửa lại label History

--- obj/Debug/net9.0/ItalisaTools.AssemblyInfo.cs
+++ obj/Debug/net9.0/ItalisaTools.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ItalisaTools")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9266d168b14bf84a21c7ee6e2ee70c3e0715894")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7837564ebc138e1fef9dc58f8fef5e9dc6f5db2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ItalisaTools")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ItalisaTools")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- obj/Release/net9.0/win-x64/ItalisaTools.AssemblyInfo.cs
+++ obj/Release/net9.0/win-x64/ItalisaTools.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ItalisaTools")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c048af201c8b0e8daac894bc8929eb9e63f46c5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7837564ebc138e1fef9dc58f8fef5e9dc6f5db2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ItalisaTools")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ItalisaTools")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
