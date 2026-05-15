24d8a638802042d2105d317cff895fbf38201aff
minhchien137 <bmc13072001@gmail.com>
2026-05-15 15:53:30 +0700

    demo compare system and excel file

---
+++ Controllers/ComparisonController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ItalisaTools.Controllers
+{
+    public class ComparisonController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
