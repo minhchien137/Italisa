e6036a15874c2eb0d2955fe95f9a1e9a7f6d2027
minhchien137 <bmc13072001@gmail.com>
2026-05-19 09:10:04 +0700

    fix lại upload

--- obj/Debug/net9.0/ItalisaTools.AssemblyInfo.cs
+++ obj/Debug/net9.0/ItalisaTools.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ItalisaTools")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e1b7ef737ae298c23f740edb6f3b38f760a68b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71b31823c315ad85a6f9e7978875e8efb31fd212")]
 [assembly: System.Reflection.AssemblyProductAttribute("ItalisaTools")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ItalisaTools")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
